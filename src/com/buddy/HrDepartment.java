3614c6c3ab9609a6167bb1ac07d1ba88ecd9b476
kauramanp <33446281+kauramanp@users.noreply.github.com>
2022-10-08 20:02:13 +0530

    Initial commit

---
+++ src/com/buddy/HrDepartment.java
@@ -0,0 +1,16 @@
+package com.buddy;
+
+public class HrDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "Hr Department";
+	}
+	public String getTodaysWork() {
+		return "Fill todays's timesheet and mark your attendance";
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	public String doActivity() {
+		return "team Lunch";
+	}	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module Amanpreet_FOPJava_GradedProject1 {
+}
